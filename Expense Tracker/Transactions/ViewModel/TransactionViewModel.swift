b1fe3e91b68e6372f367a775cb0768940dced8b8
Rasmus Antsi <rasmus435@icloud.com>
2025-07-17 23:00:51 +0300

    File Organization

---
+++ Expense Tracker/Transactions/ViewModel/TransactionViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  TransactionViewModel.swift
+//  Expense Tracker
+//
+//  Created by Rasmus Antsi on 15.07.2025.
+//
+
+import Foundation
+import SwiftData
+
+@MainActor
+class TransactionViewModel: ObservableObject {
+    @Published var transactions: [Transaction] = []
+    
+    private var context: ModelContext
+    
+    init(context: ModelContext) {
+        self.context = context
+        fetchTransactions()
+    }
+    
+    var balance: Double {
+        transactions.reduce(0) { $0 + ($1.isIncome ? $1.amount : -$1.amount) }
+    }
+    
+    func fetchTransactions() {
+            let descriptor = FetchDescriptor<Transaction>(sortBy: [SortDescriptor(\.date, order: .reverse)])
+            do {
+                transactions = try context.fetch(descriptor)
+            } catch {
+                print("❌ Fetch failed: \(error)")
+            }
+        }
+
+    func addTransaction(_ transaction: Transaction) {
+        context.insert(transaction)
+        try? context.save()
+        fetchTransactions()
+    }
+
+    func deleteTransaction(_ transaction: Transaction) {
+        context.delete(transaction)
+        try? context.save()
+        fetchTransactions()
+    }
+}
